7fc3bc24ed4f68f1d5ceed6a59fc87031035b998
Katerina <katerina.loschinina@ft.com>
2018-05-03 22:13:53 +0100

    add image with the logo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './images/logo/0-logo.png';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <header>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<img className="front-page-logo" src={logo} />
+			</div>
+		);
+	}
 }
 
 export default App;

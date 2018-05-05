508dc8cca0146dcbb20d106394cae02bb9f35a21
Katerina <katerina.loschinina@ft.com>
2018-05-05 18:58:28 +0100

    add some comments

--- src/projects/bird/planetSettings.js
+++ src/projects/bird/planetSettings.js
@@ -2,9 +2,9 @@ import mainImage from './bird-main-image.png';
 import blurredImage from './bird-blurred-image.png';
 
 const style = {
-    top: '20vh',
-    left: '20vw',
-    width: '10vw'
+    top: '20vh', // from the top, % view height
+    left: '20vw', // from the left, % view width
+    width: '10vw' // width, % view width
 }
 
 export default {

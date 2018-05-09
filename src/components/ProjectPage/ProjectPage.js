24d616390d9cdb22caf43625dfe6c8c397ebe553
Katerina <katerina.loschinina@ft.com>
2018-05-09 19:48:34 +0100

    create ProjectPage component and connect it to router

---
+++ src/components/ProjectPage/ProjectPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './ProjectPage.css';
+
+class ProjectPage extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Symbiosis</h1>
+			</div>
+		);
+	}
+}
+
+export default ProjectPage;

4d7e5dc797b00d64614f629bc87c33fcfe3e0c42
Katerina <katerina.loschinina@ft.com>
2018-05-17 21:19:47 +0100

    ProjectPage.js selects required props based on the project state and generates a page

--- src/components/ProjectPage/ProjectPage.js
+++ src/components/ProjectPage/ProjectPage.js
@@ -3,39 +3,37 @@ import ReactPlayer from 'react-player';
 import ImageGallery from 'react-image-gallery';
 
 import './ProjectPage.css';
+import projects from './../../../src/projects/index'
+import flower from './../../projects/symbiosis/gallery/flower.jpg'
 
 class ProjectPage extends Component {
 	render() {
-		const images = [
-			{
-				original: 'https://picsum.photos/200/300',
-				thumbnail: 'https://picsum.photos/200/300'
-			},
-			{
-				original: 'https://picsum.photos/200/300',
-				thumbnail: 'https://picsum.photos/200/300'
-			},
-			{
-				original: 'https://picsum.photos/200/300',
-				thumbnail: 'https://picsum.photos/200/300'
-			}
-		];
+		const selectedProject = Object.keys(projects).find(key => {
+			return projects[key].projectName === this.props.match.params.projectName
+		})
+		const pageData = projects[selectedProject].data.projectPage
 
 		return (
 			<div className="project-container">
-				<h1>Symbiosis</h1>
-				<hr></hr>
-				<p>
-					The project «Symbiosis» is an interactive installation experience. The aim is to merge technology with art to create an organic experience that connects people on a primordial level, through bringing awareness to the connecting principle of breath.
-				</p>
-				<p>
-					Below is the development of the project from the recent to the first prototype. In the current developments we are using pressure sensors and specially designed seats to support posture and meditative experience, and developing synchronisation software that will simulate environmental response from collective synced breathing rhythm.
-				</p>
-				<p>
-					Symbiosis #6 at Lightwaves, Salford Quays, Manchester December 2017
-				</p>
-				<ReactPlayer url='https://vimeo.com/251900220' />
-				<ImageGallery items={images} />
+			{ pageData.map(pageElement => {
+				switch (pageElement.tag) {
+					case 'header':
+						return <h1>{pageElement.text}</h1>
+						break
+					case 'paragraph':
+						return <p>{pageElement.text}</p>
+					case 'video':
+						return <div>
+							<p>{pageElement.highlight}</p>
+							<ReactPlayer url={pageElement.url} />
+						</div>
+					case 'gallery':
+						return <div>
+							<p>{pageElement.highlight}</p>
+							<ImageGallery items={pageElement.images} />
+						</div>
+				}
+			}) }
 			</div>
 		);
 	}

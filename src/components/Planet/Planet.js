58b8d54c38b599875946378c3ba0f333850e251f
Katerina <katerina.loschinina@ft.com>
2018-05-18 20:39:21 +0100

    remove semicolons from Planet.js

--- src/components/Planet/Planet.js
+++ src/components/Planet/Planet.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
-import './Planet.css';
+import React, { Component } from 'react'
+import './Planet.css'
 
 class Planet extends Component {
 	constructor(props) {
-		super(props);
+		super(props)
 		this.state = {
 			imgSrc: this.props.mainImage
-		};
-		this.handleMouseOver = this.handleMouseOver.bind(this);
-		this.handleMouseOut = this.handleMouseOut.bind(this);
+		}
+		this.handleMouseOver = this.handleMouseOver.bind(this)
+		this.handleMouseOut = this.handleMouseOut.bind(this)
 	}
 
 	handleMouseOver () {
 		this.setState({
 			imgSrc: this.props.blurredImage
-		});
+		})
 	}
 	
 	handleMouseOut () {
 		this.setState({
 			imgSrc: this.props.mainImage
-		});
+		})
 	}
 	
 	render() {
@@ -30,8 +30,8 @@ class Planet extends Component {
 				onMouseOut={this.handleMouseOut}
 				src={this.state.imgSrc}
 				style={this.props.style} />
-		);
+		)
 	}
 }
 
-export default Planet;
+export default Planet

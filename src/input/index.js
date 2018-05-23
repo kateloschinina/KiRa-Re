e22d76e940d460498b100530b6f5f8769794c29c
Katerina <katerina.loschinina@ft.com>
2018-05-23 21:42:08 +0100

    add categories to input index

--- src/input/index.js
+++ src/input/index.js
@@ -1,9 +1,10 @@
 import projects from './projects'
+import categories from './categories'
 
 const input = {
     projects: projects,
-    categories: [ ]
+    categories: categories
 }
 
 
-export default projects
+export default input

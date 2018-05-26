8c50e6c5d005280dd7f67af54fda53a0d81b7906
Katerina <katerina.loschinina@ft.com>
2018-05-26 13:09:17 +0100

    add sample of the background

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -8,7 +8,7 @@ import logo from "./../../images/logo/0-logo.png"
 class App extends Component {
     render() {
         return (
-            <div>
+            <div className="landing-page">
                 <img className="front-page-logo" src={logo} alt="logo" />
                 {/* <Planet style={projectBird.style} mainImage={projectBird.mainImage} blurredImage={projectBird.blurredImage} /> */}
                 <Graph />

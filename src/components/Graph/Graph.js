67dc1206542b9f04000550f817005bfd7e8a0a92
Ekaterina Loshchinina <ekaterina.loshchinina@gmail.com>
2018-05-27 15:47:05 +0100

    Landing page (#10)
    * prettify graph
    
    * add background pictures samples
    
    * move Graph related css to Graph section
    
    * add sample of the background
    
    * play a bit with graph settings
    
    * creating initial draft of the header
    
    * create initial version of instructions section

--- src/components/Graph/Graph.js
+++ src/components/Graph/Graph.js
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 import { Redirect } from "react-router"
 
 import vis from "vis"
+import "./Graph.css"
 
 import graphData from "./graphHelpers"
 import edgesWeWant from "./graphInput"
@@ -45,36 +46,45 @@ class App extends Component {
 
         const events = {
             doubleClick: event => {
-				const nodeId = event.nodes[0]
-				
-				if (nodeId > graphData.graph.nodes.length) {
-					if (nodeId <= graphData.allInputArray.length) {
-						this.setState({redirect: true})
-						this.setState({redirectTo: `/${graphData.allInputArray[nodeId - 1].name}`})
-						this.setState({projectProps: graphData.allInputArray[nodeId - 1].data.projectPage})
-					}
-				} else {
-					edgesWeWant.filter(edge => edge.to).forEach(edge => {
-						if (edge.from === nodeId) {
-							if (edge.to > graphData.graph.nodes.length) {
-								try {
-									this.nodes.add([
-										{
-											id: edge.to,
-											shape: "circularImage",
-											image:
-												graphData.allInputArray[edge.to - 1]
-													.data.planet,
-											size: 30
-										}
-									])
-								} catch (error) {
-									console.log(error)
-								}
-							}
-						}
-					})
-				}
+                const nodeId = event.nodes[0]
+
+                if (nodeId > graphData.graph.nodes.length) {
+                    if (nodeId <= graphData.allInputArray.length) {
+                        this.setState({ redirect: true })
+                        this.setState({
+                            redirectTo: `/${
+                                graphData.allInputArray[nodeId - 1].name
+                            }`
+                        })
+                        this.setState({
+                            projectProps:
+                                graphData.allInputArray[nodeId - 1].data
+                                    .projectPage
+                        })
+                    }
+                } else {
+                    edgesWeWant.filter(edge => edge.to).forEach(edge => {
+                        if (edge.from === nodeId) {
+                            if (edge.to > graphData.graph.nodes.length) {
+                                try {
+                                    this.nodes.add([
+                                        {
+                                            id: edge.to,
+                                            shape: "circularImage",
+                                            image:
+                                                graphData.allInputArray[
+                                                    edge.to - 1
+                                                ].data.planet,
+                                            size: 30
+                                        }
+                                    ])
+                                } catch (error) {
+                                    console.log(error)
+                                }
+                            }
+                        }
+                    })
+                }
             }
         }
 
@@ -87,7 +97,7 @@ class App extends Component {
                 {this.state.redirect ? (
                     <Redirect to={this.state.redirectTo} />
                 ) : null}
-                <div id="interactive-graph"></div>
+                <div id="interactive-graph" />
             </div>
         )
     }
--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -4,12 +4,26 @@ import "./LandingPage.css"
 import Graph from "./../Graph/Graph"
 
 import logo from "./../../images/logo/0-logo.png"
+import pointer from "./../../images/background/pointer.png"
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <img className="front-page-logo" src={logo} alt="logo" />
+            <div className="landing-page">
+                <div className="header">
+                    <img className="front-page-logo animate-logo" src={logo} alt="logo" />
+                    <img className="front-page-pointer animate-logo" src={pointer} alt="pointer" />
+                    <h2 className="menu-item menu-item-first menu-item-selected">map of my consiousness</h2>
+                    <h2 className="menu-item">dump of all projects</h2>
+                    <h2 className="menu-item">about KiRa</h2>
+                </div>
+                <div className="instructions">
+                    <p className="instructions-heading">how to navigate the map</p>
+                    <p className="instructions-text">observe</p>
+                    <p className="instructions-text">to see projects double click on the category</p>
+                    <p className="instructions-text">to see project details double click on the project</p>
+                    <p className="instructions-text">be happy</p>
+                </div>
                 {/* <Planet style={projectBird.style} mainImage={projectBird.mainImage} blurredImage={projectBird.blurredImage} /> */}
                 <Graph />
             </div>

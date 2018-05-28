484bc11b12aea9517c75e96a01ceab78c1728b24
Katerina <katerina.loschinina@ft.com>
2018-05-28 18:05:14 +0100

    save the work on ProjectPage as have to switch to a different branch

--- src/components/ProjectPage/ProjectPage.js
+++ src/components/ProjectPage/ProjectPage.js
@@ -4,6 +4,7 @@ import ImageGallery from "react-image-gallery"
 
 import "./ProjectPage.css"
 import input from "./../../../src/input/index"
+import logo from "./../../images/logo/0-logo.png"
 
 class ProjectPage extends Component {
     render() {
@@ -15,41 +16,51 @@ class ProjectPage extends Component {
         const pageData = input.projects[selectedProject].data.projectPage
 
         return (
-            <div className="project-container">
-                {pageData.map((pageElement, key) => {
-                    switch (pageElement.tag) {
-                        case "header":
-                            return (
-                                <h1 key={`header-${key}`}>
-                                    {pageElement.text}
-                                </h1>
-                            )
-                        case "paragraph":
-                            return (
-                                <p key={`paragraph-${key}`}>
-                                    {pageElement.text}
-                                </p>
-                            )
-                        case "video":
-                            return (
-                                <div key={`video-${key}`}>
-                                    <p>{pageElement.highlight}</p>
-                                    <ReactPlayer url={pageElement.url} />
-                                </div>
-                            )
-                        case "gallery":
-                            return (
-                                <div key={`gallery-${key}`}>
-                                    <p>{pageElement.highlight}</p>
-                                    <ImageGallery items={pageElement.images} />
-                                </div>
-                            )
-                        default:
-                            return (
-                                <p key={`paragraph-${key}`}>KiRa is awesome</p>
-                            )
-                    }
-                })}
+            <div className="project-page">
+                <div className="project-container">
+                    <div className="link-to-home">
+                        <a href="/">
+                            <h2>back to ----></h2>
+                            <img className="project-page-logo" src={logo} alt="logo" />
+                        </a>
+                    </div>
+                    <div className="project-description">
+                        {pageData.map((pageElement, key) => {
+                            switch (pageElement.tag) {
+                                case "header":
+                                    return (
+                                        <h1 key={`header-${key}`}>
+                                            {pageElement.text}
+                                        </h1>
+                                    )
+                                case "paragraph":
+                                    return (
+                                        <p key={`paragraph-${key}`}>
+                                            {pageElement.text}
+                                        </p>
+                                    )
+                                case "video":
+                                    return (
+                                        <div key={`video-${key}`}>
+                                            <p>{pageElement.highlight}</p>
+                                            <ReactPlayer url={pageElement.url} />
+                                        </div>
+                                    )
+                                case "gallery":
+                                    return (
+                                        <div key={`gallery-${key}`}>
+                                            <p>{pageElement.highlight}</p>
+                                            <ImageGallery items={pageElement.images} />
+                                        </div>
+                                    )
+                                default:
+                                    return (
+                                        <p key={`paragraph-${key}`}>KiRa is awesome</p>
+                                    )
+                            }
+                        })}
+                    </div>
+                </div>
             </div>
         )
     }

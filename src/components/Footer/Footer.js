6e534eb02fe3b82be4cb5bc053438a9d6bbeee4e
Ekaterina Loshchinina <ekaterina.loshchinina@gmail.com>
2018-06-13 14:08:11 +0100

    Footer (#18)
    * add a facebook icon
    
    * add footer component to a landing page
    
    * remove redundant code

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react"
+import "./Footer.css"
+import facebook from "./../../images/icons/facebook_icon.png"
+
+class App extends Component {
+    render() {
+        return (
+            <div className="footer--container">
+				<div className="footer">
+					<p>&copy; KiRa, 2018</p><a href="https://www.facebook.com/kirazhi"><img src={facebook}  alt="facebook" /></a>
+				</div>
+            </div>
+        )
+    }
+}
+
+export default App

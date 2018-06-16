addcbad6a739ffd110c660f5689fba3cc707efdd
Katerina <katerina.loschinina@ft.com>
2018-06-16 17:06:18 +0100

    fix alienated musicVideos

--- src/input/categories/index.js
+++ src/input/categories/index.js
@@ -2,7 +2,6 @@ import filmVideo from './film-video.png'
 import fashion from './fashion.png'
 import lco from './lco.png'
 import holographic from './holographic.png'
-import musicVideo from './music-video.png'
 import performance from './performance.png'
 import installation from './installation.png'
 import promovideos from './promovideos.png'
@@ -28,10 +27,6 @@ const categories = [
     {
         name: 'holographic',
         image: holographic
-    },
-    {
-        name: 'musicVideo',
-        image: musicVideo
     },
     {
         name: 'performance',

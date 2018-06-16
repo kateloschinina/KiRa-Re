77c769b780af9985bef682cb52c0230badc73133
Katerina <katerina.loschinina@ft.com>
2018-06-16 19:12:14 +0100

    remove non used variables

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react"
-import ReactPlayer from "react-player"
 
 import "./AboutPage.css"
-import input from "./../../../src/input/index"
 
 class AboutPage extends Component {
     render() {

86a447b347af415d46bce914b013166ff9145855
Katerina <katerina.loschinina@ft.com>
2018-06-26 00:13:48 +0100

    add switch text

--- src/components/Switch/Switch.js
+++ src/components/Switch/Switch.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
-import dot from "./../../images/background/dot.png"
 import './Switch.css'
+
+import dot from "./../../images/background/dot.png"
+import allProjects from "./../../images/menu/all-projects.png"
 
 class Switch extends Component {	
 	render() {
 		return (
 			<div className="switch__container">
 				<img className="switch__rotating-dot" src={dot} alt="rotating-dot" />
+				<img className="switch__text" src={allProjects} alt="all-projects-text" />
 			</div>
 		)
 	}

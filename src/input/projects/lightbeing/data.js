2c6926a91fa98fef77320ceb43a61cbdd1eb2e2d
Ekaterina Loshchinina <ekaterina.loshchinina@gmail.com>
2018-06-26 00:56:45 +0100

    More information on projects (#36)
    * triedchanges
    
    * cleanedgraph
    
    * addedsumprojects
    
    * projects
    
    * projectschanges
    
    * fix1

--- src/input/projects/lightbeing/data.js
+++ src/input/projects/lightbeing/data.js
@@ -7,12 +7,22 @@ import planet from './lightbeing-planet.png'
 const projectPage = [
 	{
 		tag: 'header',
-		text: 'Life In Balance'
+		text: 'Ephemeral Lightness of Being'
 	},
 	{
 		tag: 'paragraph',
-		text: 'TBD'
-	}
+		text: 'The piece explores the concept of the self in its different levels, such as human energy field and the human emotional journey that exists as a shape-shifting color light trail, temporarily created in collective space. The work meditates on the original state of nature that is driven by chaos, organizing into fractal beams of consciousness.'
+	},
+	{
+		tag: 'paragraph',
+		text: 'This is a video recording of the improvised live holographic dance piece, which won MAMAs Performance Award'
+	},
+	{
+		tag: 'video',
+		url: 'https://vimeo.com/28814091',
+		highlight: '@Kinetica ArtFair 2011'
+	},
+	
 ] 
 
 export default {
--- src/input/projects/nebulae/data.js
+++ src/input/projects/nebulae/data.js
@@ -3,6 +3,9 @@ import planet from './nebulae-planet.jpg'
 // images
 // IMPORT IMAGES FROM GALLERY USING THE FOLLOWING FORMAT
 // import [imageName] from './gallery/[imageName].jpg'
+import nebulae1 from './gallery/nebulae1.jpg'
+import nebulae2 from './gallery/nebulae2.jpg'
+import nebulae3 from './gallery/nebulae3.jpg'
 
 const projectPage = [
 	{
@@ -11,8 +14,39 @@ const projectPage = [
 	},
 	{
 		tag: 'paragraph',
-		text: 'TBD'
-	}
+		text: 'These kinetic sculptures were created originally for Secret Cinema "Star Wars", then developed. In the video presented together with "Accord" interactive sound installation'
+	},
+	{
+		tag: 'gallery',
+		highlight: 'Nebulae',
+		images: [
+			{
+				original: nebulae1,
+				thumbnail: nebulae1,
+				
+		   }, 
+		   {
+			original: nebulae2,
+			thumbnail: nebulae2,
+			
+	   },
+	   {
+		original: nebulae3,
+		thumbnail: nebulae3,
+		
+   },
+   {
+	tag: 'video',
+	url: 'https://vimeo.com/218715113',
+	highlight: '"Nebulae" and "Accord"'
+},
+
+
+
+	
+	   ]
+   }
+	
 ] 
 
 export default {

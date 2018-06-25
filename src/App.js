c5b94c102997e0c939a8bb5c2afd042410ad9b5e
Katerina <katerina.loschinina@ft.com>
2018-06-25 23:28:26 +0100

    remove redundant code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react'
-// import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css'
 
 import LandingPage from './components/LandingPage/LandingPage'
-// import ProjectPage from './components/ProjectPage/ProjectPage'
-// import AboutPage from './components/AboutPage/AboutPage'
 
 class App extends Component {
 	render() {

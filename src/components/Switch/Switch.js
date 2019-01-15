e45508db16948dde2d33e5c9c3f9d94fe7bbc93d
Katerina Loschinina <ekaterina.loshchinina@gmail.com>
2019-01-15 20:00:16 +0200

    use icons for switch

--- src/components/Switch/Switch.js
+++ src/components/Switch/Switch.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 import './Switch.css'
 
 import dot from "./../../images/background/dot.png"
-import allProjects from "./../../images/menu/all-projects-sq.png"
-import interactive from "./../../images/menu/interactive-sq.png"
+import allProjects from "./../../images/icons/all-projects-icon.svg"
+import interactive from "./../../images/icons/interactive-icon.svg"
 
 class Switch extends Component {
 	render() {
@@ -12,9 +12,9 @@ class Switch extends Component {
 			<div className="switch__container" onClick={e => this.props.changeStateTo(e, changeTo)}>
 				<img className="switch__rotating-dot" src={dot} alt="rotating-dot" />
 				{(this.props.show === "interactiveMap") ? (
-					<img className="switch__text" src={allProjects} alt="all-projects-text" />
+					<img className="switch__icon" src={allProjects} alt="all-projects-text" />
 				) : (
-					<img className="switch__text" src={interactive} alt="interactive-text" />
+					<img className="switch__icon" src={interactive} alt="interactive-text" />
 				)}
 			</div>
 		)

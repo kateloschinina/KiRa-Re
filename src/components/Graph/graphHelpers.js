8ce16187042f308bfa86f0d212da1f7a3f667243
Katerina Loschinina <ekaterina.loshchinina@gmail.com>
2019-02-06 22:44:32 +0000

    add shadow to the graph

--- src/components/Graph/graphHelpers.js
+++ src/components/Graph/graphHelpers.js
@@ -42,7 +42,7 @@ const graphData = {
     },
     options: {
         nodes: {
-            borderWidth: 2,
+            borderWidth: 1,
             borderWidthSelected: 2,
             size: 30,
             color: {
@@ -56,6 +56,13 @@ const graphData = {
                     border: "#555",
                     background: "#000"
                 }
+            },
+            shadow: {
+                enabled: true,
+                color: 'rgba(255,255,255,1)',
+                size: 60,
+                x: 0,
+                y: 0
             },
             font: { color: "#eeeeee" }
         },

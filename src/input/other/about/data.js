1a3980bb9d6f4985462eadb001cd965f3b6d4a50
Kira Zhigalina <Kira@Kiras-MBP.broadband>
2020-09-09 10:18:42 +0100

    sieben/CV

--- src/input/other/about/data.js
+++ src/input/other/about/data.js
@@ -9,16 +9,424 @@ const projectPage = [
 	},
 	{
 		tag: 'paragraph',
-		text: 'Ki Ra (Kira Zhigalina) was born in Moscow, lives and works in London, UK. She works across mediums including film, video, sound, installation, and holographic projection. She has created installations and visual projections for Secret Cinema, London Contemporary Orchestra, London Zoo, Musion Holographic Systems, Tate Modern, onedotzero, Vivienne Westwood show, The Leaf Label, Fruit for the Apocalypse, Unfashion Show, and the Rambert Dance Company, among others.'
+		text: 'Ki Ra (Kira Zhigalina) was born in Moscow, lives and works in London, UK. Her medium includes film, video, sound, installation, biofeedback and holographic projection. She has created installations and visual projections for Secret Cinema, London Contemporary Orchestra, London Zoo, Musion Holographic Systems, Tate Modern, onedotzero, Vivienne Westwood show, The Leaf Label, and the Rambert Dance Company, among others.'
 	},
 	{
 		tag: 'paragraph',
-		text: 'Her work is sensual and organic, examining subconscious and conscious appreciation of forms, and visualising the space in between, where archetypes feed into representations. Her intention is to create a visual language and immersive experience that can communicate on emotional and subconscious levels.'
+		text: 'Her work is sensual and organic, examining subconscious and conscious appreciation of forms, and visualising the space in between, where archetypes feed into representations. Her intention is to create a visual language and immersive experiences that can communicate on emotional and subconscious levels.'
 	},
 	{
 		tag: 'paragraph',
-		text: 'Ki Ra won the Kinetica Award 2012, and Award for Performance at MAMAs 2010, and was  in the top ten nominees for the Randstad Gold VJ Award in 2010.'
-	}
+		text: '.'
+	},
+	{
+		tag: 'paragraph',
+		text: '.'
+	},
+	{
+		tag: 'paragraph',
+		text: '.'
+	},
+
+	{
+		tag: 'paragraph',
+		text: 'CV'
+	},
+	{
+		tag: 'paragraph',
+		text: '.'
+	},
+	{
+		tag: 'paragraph',
+		text: 'Awards'
+	},
+	{
+		tag: 'paragraph',
+		text: 'SHINE Emerging Artist Award 2017 for Symbiosis'
+	},
+	{
+		tag: 'paragraph',
+		text: 'Kinetica Award 2012 (for short holographic video Anima Mundi)'
+	},
+	{
+		tag: 'paragraph',
+		text: 'MAMAs Award for Performance 2010 (for holographic performance “Ephemeral Lightness of Being”)'
+	},
+	{
+		tag: 'paragraph',
+		text: '.'
+	},
+	{
+		tag: 'paragraph',
+		text: 'Screenings and Exhibitions'
+	},
+	{
+		tag: 'paragraph',
+		text: 'October ’19    Mantra Festival, Brick lane'
+	},
+	{
+		tag: 'paragraph',
+		text: 'June ’19       Great Exhibition Road Festival'
+	},
+	{
+		tag: 'paragraph',
+		text: 'March ’19      NONDUAL, Hoxton Cabin'
+	},
+	{
+		tag: 'paragraph',
+		text: 'February ’19....Nottingham Light Festival'
+	},
+	{
+		tag: 'paragraph',
+		text: 'October ’18.....Leeds Light Night'
+	},
+	{
+		tag: 'paragraph',
+		text: 'September ’18   Fort Process, Newhaven'
+	},
+	{
+		tag: 'paragraph',
+		text: 'June ’18        Birmingham International Dance Festival (Symbiosis)'
+	},
+	{
+		tag: 'paragraph',
+		text: 'May ’18         FLUX Exhibition, Ugly Duck, London (Symbiosis)'
+	},
+	{
+		tag: 'paragraph',
+		text: 'December ’17    Lightwaves, Salford Quays, Manchester (Symbiosis)'
+	},
+	{
+		tag: 'paragraph',
+		text: 'October ’17     Leeds Light Night (Symbiosis)'
+	},
+	{
+		tag: 'paragraph',
+		text: 'September ’17   Liverpool Psychedelic Festival'
+	},
+	{
+		tag: 'paragraph',
+		text: 'August ’17      ASSMBLY, Leeds'
+	},
+	{
+		tag: 'paragraph',
+		text: 'June ’17        Real/Virtual Festival, Ugly Duck, London'
+	},
+	{
+		tag: 'paragraph',
+		text: 'May ’17          We are Now Festival Launch, London'
+	},
+	{
+		tag: 'paragraph',
+		text: 'May ’17          Harmony is Discordant Accord, Solo Exhibition at Maxilla Space'
+		},
+		{
+			tag: 'paragraph',
+			text: 'November ’16  Symposium on New Technologies for Mindful Awareness and Wellbeing, BCS, London'
+		},
+		{
+			tag: 'paragraph',
+			text: 'June ’16      InTransit Festival of the Arts, London'
+		},
+		{
+			tag: 'paragraph',
+			text: 'March ’16     Maxilla Open Studios, London'
+		},
+		{
+			tag: 'paragraph',
+			text: 'February ’16  Incloodu Festival for the Deaf, RichMix, London'
+		},
+		{
+			tag: 'paragraph',
+			text: 'September ’15  Alternative Fashion Week, Berlin'
+		},
+		{
+			tag: 'paragraph',
+			text: 'July ’15       Breaking Convention 2015, Greenwich University'
+		},
+		{
+			tag: 'paragraph',
+			text: 'May ’15        Self-organised Nepal Fundraiser ’Sing for Nepal’ at Coate Studios'
+		},
+		{
+			tag: 'paragraph',
+			text: 'July ’14       LSO St Lukes, "Contemplation" multi-sensory concert'
+		},
+		{
+			tag: 'paragraph',
+			text: 'May ’14        Re-Cycle Style, Hundred Years Gallery, London'
+		},
+		{
+			tag: 'paragraph',
+			text: 'December ’13   Screening at Ethical Fashion Japan, Tokyo'
+		},
+		{
+			tag: 'paragraph',
+			text: 'January ’13    `Re-Cycle Style` Exhibition, Shop 172, Brick Lane, London'
+		},
+		{
+			tag: 'paragraph',
+			text: 'September ’12   Festival `Apparition`, Artplay Centre for Design, Moscow'
+		},
+		{
+			tag: 'paragraph',
+			text: 'August    ’12   Tweet-me-up at The Tanks, Tate Modern'
+		},
+		{
+			tag: 'paragraph',
+			text: 'February ’12    Kinetica Art Fair 2012, London'
+		},
+		{
+			tag: 'paragraph',
+			text: 'September ’11   Waterpieces Contemporary Art & Videoart Festival, Finland'            
+		},
+		{
+			tag: 'paragraph',
+			text: 'July ’11        InTransit Festival, London'
+		},
+		{
+			tag: 'paragraph',
+			text: 'April ’11       Digital Stages Festival, London'
+		},
+		{
+			tag: 'paragraph',
+			text: 'March ’11        CologneOFF 2011 UKRAINE'
+		},
+		{
+			tag: 'paragraph',
+			text: 'March ’11        Climate Change Week, ZSL, London'
+		},
+		{
+			tag: 'paragraph',
+			text: 'March ’11        Dialects 4 `Home-made`, Pushkin House, London'
+		},
+		{
+			tag: 'paragraph',
+			text: 'February ’11     Kinetica Art Fair, London'
+		},
+		{
+			tag: 'paragraph',
+			text: 'January ’11      Responsive Ecologies Exhibition, Watermans, London'
+		},
+		{
+			tag: 'paragraph',
+			text: 'December ’10     Yota Space Festival of New Technologies, St.Petersburg'
+		},
+		{
+			tag: 'paragraph',
+			text: 'November ’10     Screening at BFI as part of onedotzero festival'
+		},
+		{
+			tag: 'paragraph',
+			text: 'October ’10       Ranstad VJ Gold Award (screening in 54 cities around the world, nominated in the top ten)'
+		},
+		{
+			tag: 'paragraph',
+			text: 'August ’10        Cologne OFF Film Festival, worldwide'
+		},
+		{
+			tag: 'paragraph',
+			text: 'December ’09      Vivienne Westwood`s ‘Active Resistance to Propaganda` Show, Bloomsbury Ballroom, London'
+		},
+		{
+			tag: 'paragraph',
+			text: 'November ’09      MAMAs Award Ceremony, Musion Academy Media Awards'
+		},
+		{
+			tag: 'paragraph',
+			text: 'August ’09         Videoholica Film Festival, Varna, Bulgaria'
+		},
+		{
+			tag: 'paragraph',
+			text: 'June ’09           Siteshow II, London'
+		},
+		{
+			tag: 'paragraph',
+			text: 'March ’09          Margo art Gallery, Moscow'
+		},
+		{
+			tag: 'paragraph',
+			text: 'January ’09        solo exhibition at BOA Building, Tokyo'
+		},
+		{
+			tag: 'paragraph',
+			text: 'October ’08        St.Petersburg International Youth Film Festival, Russia'
+		},
+		{
+			tag: 'paragraph',
+			text: '.'
+		},
+		{
+			tag: 'paragraph',
+			text: 'Commissions'
+		},
+		{
+			tag: 'paragraph',
+			text: 'July ’18            Interactive Light Installations for Noisily Festival of Music and Arts'
+		},
+		{
+			tag: 'paragraph',
+			text: 'June ’18            Promo Video for Sieben “Crumbs”'     
+		},
+		{
+			tag: 'paragraph',
+			text: ' January ’18        Art Direction and Projections for Gift of Life fundraising Gala at Savoy Hotel, London'
+		},
+		{
+			tag: 'paragraph',
+			text: 'October ’17         Interactive Light Installation for Leeds Light Night and Lightwaves'
+		},
+		{
+			tag: 'paragraph',
+			text: 'September ’17       Animation Workshop and curation of a Day of Eastern European           Animations at Muse Gallery, supported by RBKC and Portobello Film Festival'
+		},
+		{
+			tag: 'paragraph',
+			text: 'July ’17            Interactive Light Installation for Nowhere, Spain'
+		},
+		{
+			tag: 'paragraph',
+			text: 'November ’16        Projection for WordLife event at Weston Park Museum, Sheffield'
+		},
+		{
+			tag: 'paragraph',
+			text: 'October ’16         Music Video for King Capisce'
+		},
+		{
+			tag: 'paragraph',
+			text: 'August ’16          Promo Video for Re-Cycle Style IV'
+		},
+		{
+			tag: 'paragraph',
+			text: 'July ’16            Interactive Light Installation for Nowhere Festival, Spain'
+		},
+		{
+			tag: 'paragraph',
+			text: 'February-April’16   Visuals for Sound Being events @ Hoxton Basement.  - visuals projected on a 211cm gong'
+		},
+		{
+			tag: 'paragraph',
+			text: 'September ’15       Promo Video for Re-Cycle Style III, - collaboration with butoh performers, photographer, painter, and fashion'
+		},
+		{
+			tag: 'paragraph',
+			text: 'May ’15             Installation Artist for Secret Cinema ’Star Wars'
+		},
+		{
+			tag: 'paragraph',
+			text: 'February ’15         Music Video for Vessels ’Echo In'
+		},
+		{
+			tag: 'paragraph',
+			text: 'July ’14                 Performance design and visuals for Soundhub residency at LSO St.Lukes'
+		},
+		{
+			tag: 'paragraph',
+			text: 'May ’14                Fashion promo for Re-Cycle Style II, japanese eco fashion label'
+		},
+		{
+			tag: 'paragraph',
+			text: 'October ’13         video projection for LCO Imagined Occasions, Oval Space, projected on the gasometer'
+		},
+		{
+			tag: 'paragraph',
+			text: 'May ’13                video art for LCO Imagined Occasions, Aldwych, projection on the tube station'
+		},
+		{
+			tag: 'paragraph',
+			text: 'April ’13               Promo video for Rambert Dance Company'
+		},
+		{
+			tag: 'paragraph',
+			text: 'March ’13            Installation Artist for Secret Cinema ’Brazil’'
+		},
+		{
+			tag: 'paragraph',
+			text: 'January ’13         Fashion Promo for `Re-Cycle Style`'
+		},
+		{
+			tag: 'paragraph',
+			text: 'June ’12                Installation Artist for Secret Cinema, `Prometheus`'
+		},
+		{
+			tag: 'paragraph',
+			text: 'January ’12         Photo shoot and music video for `Bertie Sees the World`'
+		},
+		{
+			tag: 'paragraph',
+			text: 'November ’11    Secret Cinema ’The Third Man’ (projections design)'
+		},
+		{
+			tag: 'paragraph',
+			text: 'July ’11        Projections designer for Common Sounds: Touching the Void,  at  Commonwealth Institute, Kensington, London    Visuals for ballet staging of Harrisson Birtwistle`s ’Orpheus Elegies’'
+		},
+	{
+		tag: 'paragraph',
+		text: 'February ’11        Visual projection designer for Secret Cinema ’The Red Shoes’'
+	},
+	{
+		tag: 'paragraph',
+		text: 'September ’10       Commission by ZSL and Musion to create a holographic video piece to widen awareness about climate change'
+	},
+	{
+		tag: 'paragraph',
+		text: 'August  ’10         Promotional video for Vessels, Cuckundoo Records'
+	},
+	{
+		tag: 'paragraph',
+		text: 'January ’10         Promotional video for Polar Bear, The Leaf Label'
+	},
+	{
+		tag: 'paragraph',
+		text: 'September ’09       Commissioned by Musion and ZSL to make a piece about endangered species'         
+	},
+	{
+		tag: 'paragraph',
+		text: 'September ’07       Visuals for `Unfashion Show` at Cochrane Theatre, London'
+		
+	},
+	{
+		tag: 'paragraph',
+		text: '.'
+	},
+	{
+		tag: 'paragraph',
+		text: 'Talks'
+	},
+	{
+		tag: 'paragraph',
+		text: 'September 19         Artist Talk at FLUX Events, Lights of Soho'
+	},
+	{
+		tag: 'paragraph',
+		text: 'April 2017           Artist Talk at FLUX Events, Lights of Soho'
+	},
+	{
+		tag: 'paragraph',
+		text: 'February 2017    Artist Talk at Seven of Clubs, ZIMA'
+	},
+	{
+		tag: 'paragraph',
+		text: '.'
+	},
+	{
+		tag: 'paragraph',
+		text: 'Education'
+	},
+	{
+		tag: 'paragraph',
+		text: '2005-2008  Central Saint Martins College of Art and Design, London'                                                 
+	},
+	{
+		tag: 'paragraph',
+		text: 'BA Fine Art, 4D specialization (time - based media, short course in 16mm)'
+	},
+	{
+		tag: 'paragraph',
+		text: '2004-2005  Central Saint Martins College of Art Design Foundation (moving image specialization)'
+	},
+		 
+		
 ] 
 
 export default {
